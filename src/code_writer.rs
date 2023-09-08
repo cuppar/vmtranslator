cdf71853d119269964a0077f06b97f93f8b0169d
Cuppar <cuppar@163.com>
2023-09-08 13:58:02 +0800

    complete chapter 07

--- src/code_writer.rs
+++ src/code_writer.rs
@@ -7,6 +7,7 @@ use std::{
 
 pub struct CodeWriter {
     file: File,
+    filename: String,
     mem_seg_map: HashMap<String, String>,
 }
 
@@ -22,13 +23,24 @@ impl CodeWriter {
             ("argument".to_string(), "ARG".to_string()),
             ("this".to_string(), "THIS".to_string()),
             ("that".to_string(), "THAT".to_string()),
-            ("temp".to_string(), "TEMP".to_string()),
         ]);
 
-        Ok(Self { file, mem_seg_map })
+        let filename = path
+            .file_name()
+            .ok_or(io::Error::new(io::ErrorKind::NotFound, "no filename"))?
+            .to_str()
+            .unwrap()
+            .to_string();
+
+        Ok(Self {
+            file,
+            filename,
+            mem_seg_map,
+        })
     }
 
     fn _write_arithmetic(&mut self, cmd: &str, id: &str) -> String {
+        const TEMP_BASE: i32 = 5;
         match cmd {
             "add" => {
                 String::new()
@@ -36,10 +48,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 = temp1 + temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D+M\n" // D: x, M: y
                     + "// end ======= temp0 = temp1 + temp0\n"
                     + "\n"
@@ -53,10 +65,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 = temp1 - temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D-M\n" // D: x, M: y
                     + "// end ======= temp0 = temp1 - temp0\n"
                     + "\n"
@@ -69,7 +81,7 @@ impl CodeWriter {
                     + "// start ======= neg\n"
                     + &self._write_pop("temp", 0)
                     + "// start ======= temp0 = -temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=-M\n"
                     + "// end ======= temp0 = -temp0\n"
                     + "\n"
@@ -83,10 +95,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 := temp1 == temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D-M\n" // D: x, M: y
                     + "D=M\n" // if D(x-y)==0, M=true(-1), else M=false(0)
                     + "M=0\n" // bool=false(0)
@@ -95,7 +107,7 @@ impl CodeWriter {
                     + &format!("@CONTINUE_{}\n", id)
                     + "0;JMP\n"
                     + &format!("(HIT_{})\n", id) // test hit, bool=true(-1)
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=-1\n"
                     + &format!("(CONTINUE_{})\n", id)
                     + "// end ======= temp0 := temp1 == temp0\n"
@@ -110,10 +122,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 := temp1 > temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D-M\n" // D: x, M: y
                     + "D=M\n" // if D(x-y) > 0, M=true(-1), else M=false(0)
                     + "M=0\n" // bool=false(0)
@@ -122,7 +134,7 @@ impl CodeWriter {
                     + &format!("@CONTINUE_{}\n", id)
                     + "0;JMP\n"
                     + &format!("(HIT_{})\n", id) // test hit, bool=true(-1)
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=-1\n"
                     + &format!("(CONTINUE_{})\n", id)
                     + "// end ======= temp0 := temp1 > temp0\n"
@@ -137,10 +149,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 := temp1 < temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D-M\n" // D: x, M: y
                     + "D=M\n" // if D(x-y) < 0, M=true(-1), else M=false(0)
                     + "M=0\n" // bool=false(0)
@@ -149,7 +161,7 @@ impl CodeWriter {
                     + &format!("@CONTINUE_{}\n", id)
                     + "0;JMP\n"
                     + &format!("(HIT_{})\n", id) // test hit, bool=true(-1)
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=-1\n"
                     + &format!("(CONTINUE_{})\n", id)
                     + "// end ======= temp0 := temp1 < temp0\n"
@@ -164,10 +176,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 = temp1 & temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D&M\n" // D: x, M: y
                     + "// end ======= temp0 = temp1 & temp0\n"
                     + "\n"
@@ -181,10 +193,10 @@ impl CodeWriter {
                     + &self._write_pop("temp", 0)
                     + &self._write_pop("temp", 1)
                     + "// start ======= temp0 = temp1 | temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "A=A+1\n"
                     + "D=M\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=D|M\n" // D: x, M: y
                     + "// end ======= temp0 = temp1 | temp0\n"
                     + "\n"
@@ -197,7 +209,7 @@ impl CodeWriter {
                     + "// start ======= not\n"
                     + &self._write_pop("temp", 0)
                     + "// start ======= temp0 = !temp0\n"
-                    + "@TEMP\n"
+                    + &format!("@{TEMP_BASE}\n")
                     + "M=!M\n"
                     + "// end ======= temp0 = !temp0\n"
                     + "\n"
@@ -241,13 +253,47 @@ impl CodeWriter {
                     + &format!("// end ======== push {arg1} {arg2}\n")
                     + &"\n"
             }
-            "pointer" => {
-                //todo
+            "temp" => {
+                let ram_address = arg2 + 5;
                 String::new()
                     + &format!("// start ======== push {arg1} {arg2}\n")
-                    + &format!("// D={arg2}\n")
-                    + &format!("@{arg2}\n")
-                    + &"D=A\n"
+                    + &format!("// D={arg1}+{arg2}\n")
+                    + &format!("@{ram_address}\n")
+                    + &"D=M\n"
+                    + &format!("// stack[SP]=D\n")
+                    + &"@SP\n"
+                    + &"A=M\n"
+                    + &"M=D\n"
+                    + &format!("// SP++\n")
+                    + &"@SP\n"
+                    + &"M=M+1\n"
+                    + &format!("// end ======== push {arg1} {arg2}\n")
+                    + &"\n"
+            }
+            "static" => {
+                let static_var_id = format!("{}.{arg2}", self.filename);
+                String::new()
+                    + &format!("// start ======== push {arg1} {arg2}\n")
+                    + &format!("// D={arg1}+{arg2}\n")
+                    + &format!("@{static_var_id}\n")
+                    + &"D=M\n"
+                    + &format!("// stack[SP]=D\n")
+                    + &"@SP\n"
+                    + &"A=M\n"
+                    + &"M=D\n"
+                    + &format!("// SP++\n")
+                    + &"@SP\n"
+                    + &"M=M+1\n"
+                    + &format!("// end ======== push {arg1} {arg2}\n")
+                    + &"\n"
+            }
+            "pointer" => {
+                let pointer_address = if arg2 == 0 { 3 } else { 4 };
+                String::new()
+                    + &format!("// start ======== push {arg1} {arg2}\n")
+                    + &format!("// D= value of {arg1} {arg2}\n")
+                    + &format!("@{pointer_address}\n")
+                    + &"D=M\n"
                     + &format!("// stack[SP]=D\n")
                     + &"@SP\n"
                     + &"A=M\n"
@@ -265,7 +311,7 @@ impl CodeWriter {
                     + &format!("// start ======== push {arg1} {arg2}\n")
                     + &format!("// D={arg1}+{arg2}\n")
                     + &format!("@{arg1}\n")
-                    + &"D=A\n"
+                    + &"D=M\n"
                     + &format!("@{arg2}\n")
                     + &"D=D+A\n"
                     + &"A=D\n"
@@ -285,27 +331,59 @@ impl CodeWriter {
 
     fn _write_pop(&mut self, arg1: &str, arg2: i32) -> String {
         match arg1 {
-            "pointer" => {
-                //todo
+            "temp" => {
+                let ram_address = arg2 + 5;
                 String::new()
                     + &format!("// start ======== pop {arg1} {arg2}\n")
                     + &format!("// SP--\n")
                     + &"@SP\n"
                     + &"M=M-1\n"
                     + &format!("// R13=addr({arg1}+{arg2})\n")
-                    + &format!("@{arg1}\n")
-                    + &"D=A\n"
-                    + &format!("@{arg2}\n")
-                    + &"D=D+A\n"
+                    + &format!("@{ram_address}\n")
+                    + &"D=A\n" // temp hold the register itself, not address
                     + &"@R13\n"
                     + &"M=D\n"
                     + &format!("// D=stack[SP]\n")
                     + &"@SP\n"
                     + &"A=M\n"
                     + &"D=M\n"
-                    + &format!("// R13=D\n")
+                    + &format!("// *R13=D\n")
                     + &"@R13\n"
                     + &"A=M\n"
+                    + &"M=D\n"
+                    + &format!("// end ======== pop {arg1} {arg2}\n")
+                    + &"\n"
+            }
+            "static" => {
+                let static_var_id = format!("{}.{arg2}", self.filename);
+                String::new()
+                    + &format!("// start ======== pop {arg1} {arg2}\n")
+                    + &format!("// SP--\n")
+                    + &"@SP\n"
+                    + &"M=M-1\n"
+                    + &format!("// D=stack[SP]\n")
+                    + &"@SP\n"
+                    + &"A=M\n"
+                    + &"D=M\n"
+                    + &format!("// *{{filename}}.i=D\n")
+                    + &format!("@{static_var_id}\n")
+                    + &"M=D\n"
+                    + &format!("// end ======== pop {arg1} {arg2}\n")
+                    + &"\n"
+            }
+            "pointer" => {
+                let pointer_address = if arg2 == 0 { 3 } else { 4 };
+                String::new()
+                    + &format!("// start ======== pop {arg1} {arg2}\n")
+                    + &format!("// SP--\n")
+                    + &"@SP\n"
+                    + &"M=M-1\n"
+                    + &format!("// D=stack[SP]\n")
+                    + &"@SP\n"
+                    + &"A=M\n"
+                    + &"D=M\n"
+                    + &format!("// *({arg1} {arg2})=D\n")
+                    + &format!("@{pointer_address}\n")
                     + &"M=D\n"
                     + &format!("// end ======== pop {arg1} {arg2}\n")
                     + &"\n"
@@ -320,7 +398,7 @@ impl CodeWriter {
                     + &"M=M-1\n"
                     + &format!("// R13=addr({arg1}+{arg2})\n")
                     + &format!("@{arg1}\n")
-                    + &"D=A\n"
+                    + &"D=M\n"
                     + &format!("@{arg2}\n")
                     + &"D=D+A\n"
                     + &"@R13\n"
